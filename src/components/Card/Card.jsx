8360ad62405f90d12d0262e7a8051a5ff655b68c
AlexAthayde <57647474+AlexAthayde@users.noreply.github.com>
2023-09-26 00:57:07 +0000

    Criação do Card 2

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,11 @@
 import "./Card.css"
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props);
+    const item = props.item
+
     return <div className="card">
-    <h2>Rick Sanchez</h2>
+    <h2>{item.nome}</h2>
 
     <div className='tags'>
       <div className='tag'>Status: Vivo</div>
@@ -10,6 +13,6 @@ export default function Card() {
       <div className='tag'>Origem: Terra C-137</div>
     </div>
 
-    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+    <img src={item.imagemUrl}/>
   </div>
 }
